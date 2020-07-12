6d6f016e184a0bf631a93da7750442b1c14ab50c
Guha5277 <guha5277@yandex.ru>
2020-07-12 21:35:02 +0700

    Добавление активити с представлением продукта

---
+++ app/src/main/java/ru/guhar4k/ilfumoclient/view/ProductPageActivity.java
@@ -0,0 +1,15 @@
+package ru.guhar4k.ilfumoclient.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import ru.guhar4k.ilfumoclient.R;
+
+public class ProductPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_page);
+    }
+}
