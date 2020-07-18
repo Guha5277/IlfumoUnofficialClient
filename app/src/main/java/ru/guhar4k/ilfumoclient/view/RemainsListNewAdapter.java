5289d72a8ad9e7509582173a3ad3b1515d4c9192
Guha5277 <guha5277@yandex.ru>
2020-07-18 18:38:04 +0700

    Коммит перед изменением структуры отображения остатков

---
+++ app/src/main/java/ru/guhar4k/ilfumoclient/view/RemainsListNewAdapter.java
@@ -0,0 +1,70 @@
+package ru.guhar4k.ilfumoclient.view;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.guhar4k.ilfumoclient.R;
+import ru.guhar4k.ilfumoclient.product.Remains;
+
+class RemainsListNewAdapter extends RecyclerView.Adapter<RemainsListNewAdapter.ViewHolder> {
+    private ArrayList<Remains> remainsList;
+
+    void addAll(List<Remains> remains) {
+        remainsList.clear();
+        remainsList.addAll(remains);
+        notifyDataSetChanged();
+    }
+
+    void clear() {
+        remainsList.clear();
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutID = R.layout.remains_item_list;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(layoutID, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return remainsList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvWarehouseName;
+        TextView tvRemains;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvWarehouseName = itemView.findViewById(R.id.tv_warehouse_name);
+            tvRemains = itemView.findViewById(R.id.tv_remains);
+        }
+
+        public void bind(int position) {
+            Remains remains = remainsList.get(position);
+            String storeName = remains.getWarehouse().getAltName();
+            tvWarehouseName.setText(storeName);
+            tvRemains.setText(String.valueOf(remains.getRemains()));
+        }
+    }
+}
